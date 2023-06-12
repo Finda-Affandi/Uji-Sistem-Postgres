b1c6bd4e35ce8d656912ac008085951bd046ef50
Albertus Bima Krisnawan <bimakrisnawan.business@gmail.com>
2023-06-12 23:50:18 +0700

    Add all

--- src/main/java/com/ujisistempostgres/UjisistempostgresApplication.java
+++ src/main/java/com/ujisistempostgres/UjisistempostgresApplication.java
@@ -2,11 +2,14 @@ package com.ujisistempostgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.ujisistempostgres.controller.TutorialController;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class UjisistempostgresApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UjisistempostgresApplication.class, args);
 	}
 
---
+++ src/main/java/com/ujisistempostgres/repository/TutorialRepository.java
@@ -0,0 +1,24 @@
+package com.ujisistempostgres.repository;
+
+import com.ujisistempostgres.model.Tutorial;
+
+import java.util.List;
+
+
+public interface TutorialRepository {
+    int save(Tutorial book);
+
+    int update(Tutorial book);
+
+    Tutorial findById(Long id);
+
+    int deleteById(Long id);
+
+    List<Tutorial> findAll();
+
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+
+    int deleteAll();
+}
