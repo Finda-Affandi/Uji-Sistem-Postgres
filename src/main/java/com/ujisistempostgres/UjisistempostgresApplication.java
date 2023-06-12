e6f786e27d3d6e7e90d6d8d5e1598b8b830ce879
fann <f.affnd@gmail.com>
2023-06-12 22:27:07 +0700

    Initial commit

---
+++ src/main/java/com/ujisistempostgres/UjisistempostgresApplication.java
@@ -0,0 +1,13 @@
+package com.ujisistempostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UjisistempostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UjisistempostgresApplication.class, args);
+	}
+
+}
