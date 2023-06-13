b09e470bf5885840045857c6d5e3838f3c4011fc
Albertus Bima Krisnawan <bimakrisnawan.business@gmail.com>
2023-06-13 17:19:53 +0700

    JDBC template to JDBC

--- src/main/java/com/ujisistempostgres/controller/TutorialController.java
+++ src/main/java/com/ujisistempostgres/controller/TutorialController.java
@@ -1,5 +1,9 @@
 package com.ujisistempostgres.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -26,75 +31,125 @@ import org.springframework.web.bind.annotation.RestController;
 public class TutorialController {
 
     @Autowired
-    TutorialRepository tutorialRepository;
+    private DataSource dataSource;
 
     @GetMapping("/tutorials")
     public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
         try {
-            List<Tutorial> tutorials = new ArrayList<Tutorial>();
+            List<Tutorial> tutorials = new ArrayList<>();
 
-            if (title == null)
-                tutorialRepository.findAll().forEach(tutorials::add);
-            else
-                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement;
+            if (title == null) {
+                statement = connection.prepareStatement("SELECT * FROM tutorials");
+            } else {
+                statement = connection.prepareStatement("SELECT * FROM tutorials WHERE title LIKE ?");
+                statement.setString(1, "%" + title + "%");
+            }
+
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String tutorialTitle = resultSet.getString("title");
+                String description = resultSet.getString("description");
+                boolean published = resultSet.getBoolean("published");
+                Tutorial tutorial = new Tutorial(id, tutorialTitle, description, published);
+                tutorials.add(tutorial);
+            }
+
+            connection.close();
 
             if (tutorials.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
 
             return new ResponseEntity<>(tutorials, HttpStatus.OK);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/tutorials/{id}")
     public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
-        Tutorial tutorial = tutorialRepository.findById(id);
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM tutorials WHERE id = ?");
+            statement.setLong(1, id);
 
-        if (tutorial != null) {
-            return new ResponseEntity<>(tutorial, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String title = resultSet.getString("title");
+                String description = resultSet.getString("description");
+                boolean published = resultSet.getBoolean("published");
+                Tutorial tutorial = new Tutorial(id, title, description, published);
+                connection.close();
+                return new ResponseEntity<>(tutorial, HttpStatus.OK);
+            } else {
+                connection.close();
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (SQLException e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/tutorials")
     public ResponseEntity<String> createTutorial(@RequestBody Tutorial tutorial) {
         try {
-            tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO tutorials (title, description, published) VALUES (?, ?, ?)");
+            statement.setString(1, tutorial.getTitle());
+            statement.setString(2, tutorial.getDescription());
+            statement.setBoolean(3, tutorial.isPublished());
+
+            statement.executeUpdate();
+            connection.close();
+
             return new ResponseEntity<>("Tutorial was created successfully.", HttpStatus.CREATED);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PutMapping("/tutorials/{id}")
     public ResponseEntity<String> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
-        Tutorial _tutorial = tutorialRepository.findById(id);
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("UPDATE tutorials SET title = ?, description = ?, published = ? WHERE id = ?");
+            statement.setString(1, tutorial.getTitle());
+            statement.setString(2, tutorial.getDescription());
+            statement.setBoolean(3, tutorial.isPublished());
+            statement.setLong(4, id);
 
-        if (_tutorial != null) {
-            _tutorial.setId(id);
-            _tutorial.setTitle(tutorial.getTitle());
-            _tutorial.setDescription(tutorial.getDescription());
-            _tutorial.setPublished(tutorial.isPublished());
+            int rowsUpdated = statement.executeUpdate();
+            connection.close();
 
-            tutorialRepository.update(_tutorial);
-            return new ResponseEntity<>("Tutorial was updated successfully.", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Cannot find Tutorial with id=" + id, HttpStatus.NOT_FOUND);
+            if (rowsUpdated > 0) {
+                return new ResponseEntity<>("Tutorial was updated successfully.", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Cannot find Tutorial with id=" + id, HttpStatus.NOT_FOUND);
+            }
+        } catch (SQLException e) {
+            return new ResponseEntity<>("Cannot update tutorial.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/tutorials/{id}")
     public ResponseEntity<String> deleteTutorial(@PathVariable("id") long id) {
         try {
-            int result = tutorialRepository.deleteById(id);
-            if (result == 0) {
-                return new ResponseEntity<>("Cannot find Tutorial with id=" + id, HttpStatus.OK);
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM tutorials WHERE id = ?");
+            statement.setLong(1, id);
+
+            int rowsDeleted = statement.executeUpdate();
+            connection.close();
+
+            if (rowsDeleted > 0) {
+                return new ResponseEntity<>("Tutorial was deleted successfully.", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Cannot find Tutorial with id=" + id, HttpStatus.NOT_FOUND);
             }
-            return new ResponseEntity<>("Tutorial was deleted successfully.", HttpStatus.OK);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             return new ResponseEntity<>("Cannot delete tutorial.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -102,26 +157,46 @@ public class TutorialController {
     @DeleteMapping("/tutorials")
     public ResponseEntity<String> deleteAllTutorials() {
         try {
-            int numRows = tutorialRepository.deleteAll();
-            return new ResponseEntity<>("Deleted " + numRows + " Tutorial(s) successfully.", HttpStatus.OK);
-        } catch (Exception e) {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM tutorials");
+
+            int numRowsDeleted = statement.executeUpdate();
+            connection.close();
+
+            return new ResponseEntity<>("Deleted " + numRowsDeleted + " Tutorial(s) successfully.", HttpStatus.OK);
+        } catch (SQLException e) {
             return new ResponseEntity<>("Cannot delete tutorials.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
 
     @GetMapping("/tutorials/published")
     public ResponseEntity<List<Tutorial>> findByPublished() {
         try {
-            List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+            List<Tutorial> tutorials = new ArrayList<>();
+
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM tutorials WHERE published = ?");
+            statement.setBoolean(1, true);
+
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String title = resultSet.getString("title");
+                String description = resultSet.getString("description");
+                boolean published = resultSet.getBoolean("published");
+                Tutorial tutorial = new Tutorial(id, title, description, published);
+                tutorials.add(tutorial);
+            }
+
+            connection.close();
 
             if (tutorials.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
+
             return new ResponseEntity<>(tutorials, HttpStatus.OK);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
 }
--- src/main/java/com/ujisistempostgres/repository/JdbcTutorialRepository.java
+++ src/main/java/com/ujisistempostgres/repository/JdbcTutorialRepository.java
@@ -1,71 +1,197 @@
 package com.ujisistempostgres.repository;
 
+import com.ujisistempostgres.model.Tutorial;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.ujisistempostgres.model.Tutorial;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import javax.sql.DataSource;
 
-
-@Repository
 public class JdbcTutorialRepository implements TutorialRepository {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
+
+    public JdbcTutorialRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     public int save(Tutorial tutorial) {
-        return jdbcTemplate.update("INSERT INTO tutorials (title, description, published) VALUES(?,?,?)",
-                new Object[] { tutorial.getTitle(), tutorial.getDescription(), tutorial.isPublished() });
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO tutorials (title, description, published) VALUES (?, ?, ?)",
+                     Statement.RETURN_GENERATED_KEYS
+             )) {
+            statement.setString(1, tutorial.getTitle());
+            statement.setString(2, tutorial.getDescription());
+            statement.setBoolean(3, tutorial.isPublished());
+
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new SQLException("Creating tutorial failed, no rows affected.");
+            }
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    long id = generatedKeys.getLong(1);
+                    tutorial.setId(id);
+                    return rowsAffected;
+                } else {
+                    throw new SQLException("Creating tutorial failed, no ID obtained.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 
     @Override
     public int update(Tutorial tutorial) {
-        return jdbcTemplate.update("UPDATE tutorials SET title=?, description=?, published=? WHERE id=?",
-                new Object[] { tutorial.getTitle(), tutorial.getDescription(), tutorial.isPublished(), tutorial.getId() });
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "UPDATE tutorials SET title = ?, description = ?, published = ? WHERE id = ?"
+             )) {
+            statement.setString(1, tutorial.getTitle());
+            statement.setString(2, tutorial.getDescription());
+            statement.setBoolean(3, tutorial.isPublished());
+            statement.setLong(4, tutorial.getId());
+
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 
     @Override
     public Tutorial findById(Long id) {
-        try {
-            Tutorial tutorial = jdbcTemplate.queryForObject("SELECT * FROM tutorials WHERE id=?",
-                    BeanPropertyRowMapper.newInstance(Tutorial.class), id);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM tutorials WHERE id = ?"
+             )) {
+            statement.setLong(1, id);
 
-            return tutorial;
-        } catch (IncorrectResultSizeDataAccessException e) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    String title = resultSet.getString("title");
+                    String description = resultSet.getString("description");
+                    boolean published = resultSet.getBoolean("published");
+                    return new Tutorial(id, title, description, published);
+                } else {
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
             return null;
         }
     }
 
     @Override
     public int deleteById(Long id) {
-        return jdbcTemplate.update("DELETE FROM tutorials WHERE id=?", id);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "DELETE FROM tutorials WHERE id = ?"
+             )) {
+            statement.setLong(1, id);
+
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 
     @Override
     public List<Tutorial> findAll() {
-        return jdbcTemplate.query("SELECT * from tutorials", BeanPropertyRowMapper.newInstance(Tutorial.class));
+        List<Tutorial> tutorials = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM tutorials"
+             );
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String title = resultSet.getString("title");
+                String description = resultSet.getString("description");
+                boolean published = resultSet.getBoolean("published");
+                Tutorial tutorial = new Tutorial(id, title, description, published);
+                tutorials.add(tutorial);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return tutorials;
     }
 
     @Override
     public List<Tutorial> findByPublished(boolean published) {
-        return jdbcTemplate.query("SELECT * from tutorials WHERE published=?",
-                BeanPropertyRowMapper.newInstance(Tutorial.class), published);
+        List<Tutorial> tutorials = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM tutorials WHERE published = ?"
+             )) {
+            statement.setBoolean(1, published);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    long id = resultSet.getLong("id");
+                    String title = resultSet.getString("title");
+                    String description = resultSet.getString("description");
+                    Tutorial tutorial = new Tutorial(id, title, description, published);
+                    tutorials.add(tutorial);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return tutorials;
     }
 
     @Override
     public List<Tutorial> findByTitleContaining(String title) {
-        String q = "SELECT * from tutorials WHERE title ILIKE '%" + title + "%'";
+        List<Tutorial> tutorials = new ArrayList<>();
 
-        return jdbcTemplate.query(q, BeanPropertyRowMapper.newInstance(Tutorial.class));
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT * FROM tutorials WHERE title LIKE ?"
+             )) {
+            statement.setString(1, "%" + title + "%");
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    long id = resultSet.getLong("id");
+                    String tutorialTitle = resultSet.getString("title");
+                    String description = resultSet.getString("description");
+                    boolean published = resultSet.getBoolean("published");
+                    Tutorial tutorial = new Tutorial(id, tutorialTitle, description, published);
+                    tutorials.add(tutorial);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return tutorials;
     }
 
     @Override
     public int deleteAll() {
-        return jdbcTemplate.update("DELETE from tutorials");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "DELETE FROM tutorials"
+             )) {
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 }
