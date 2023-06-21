b097bb13dc78cf14c97ee1b46f5958a61ec307f5
Albertus Bima Krisnawan <bimakrisnawan.business@gmail.com>
2023-06-21 17:27:56 +0700

    fix app property

--- src/main/java/com/ujisistempostgres/configuration/DatabaseConfiguration.java
+++ src/main/java/com/ujisistempostgres/configuration/DatabaseConfiguration.java
@@ -1,5 +1,6 @@
 package com.ujisistempostgres.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,18 +10,32 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/db_ujisistem");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("equifax17");
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+
         return new JdbcTemplate(dataSource);
     }
 }
