f269c1481b94ea08cfcb39f025c9a684a2749d07
Albertus Bima Krisnawan <bimakrisnawan.business@gmail.com>
2023-07-07 21:17:35 +0700

    clean-code

--- src/main/java/com/ujisistempostgres/controller/ServiceController.java
+++ src/main/java/com/ujisistempostgres/controller/ServiceController.java
@@ -23,17 +23,6 @@ public class ServiceController {
         this.serviceRepository = serviceRepository;
     }
 
-//    @GetMapping("/postgres")
-//    public ResponseEntity<List<Map<String, Object>>> getAllData(@RequestHeader HttpHeaders headers) {
-//        try {
-//            String tableName = headers.getFirst("table-name");
-//            List<Map<String, Object>> dataList = serviceRepository.getAllData(tableName);
-//            return ResponseEntity.ok(dataList);
-//        } catch (Exception e) {
-//            String eMessage = "An error while retrieving data";
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-//        }
-//    }
 
     @GetMapping("/postgres")
     public ResponseEntity<String> getAllDataforService(@RequestHeader HttpHeaders headers) {
@@ -56,50 +45,6 @@ public class ServiceController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-//
-//    @GetMapping("/chooseTablePosgres")
-//    public ResponseEntity<List<String>> chooseTablePosgres(@RequestHeader HttpHeaders headers) {
-//        try {
-//            List<String> tableNames = serviceRepository.getAllTableNamesForChoose();
-//            return ResponseEntity.ok()
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .body(tableNames);
-//        } catch (Exception e) {
-//            String eMessage = "An error occurred while retrieving data";
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-//        }
-//    }
-//
-//    @GetMapping("/getByTablePostgres")
-//    public ResponseEntity<List<Map<String, Object>>> getByTablePostgres(
-//            @RequestHeader HttpHeaders headers,
-//            @RequestHeader("param") String param) {
-//        try {
-//            String[] tableNames = serviceRepository.getAllTableNamesForSelectByTable();
-//            List<Map<String, Object>> result = new ArrayList<>();
-//
-//            int paramIndex = Integer.parseInt(param) - 1;
-//
-//            if (paramIndex >= 0 && paramIndex < tableNames.length) {
-//                String tableName = tableNames[paramIndex];
-//
-//                Map<String, Object> paramMap = new HashMap<>();
-//                List<List<Map<String, Object>>> dataLists = serviceRepository.getBothData(Collections.singletonList(tableName));
-//                paramMap.put("Data : ", dataLists);
-//                result.add(paramMap);
-//            } else {
-//                Map<String, Object> paramMap = new HashMap<>();
-//                paramMap.put("param", param);
-//                paramMap.put("message", "Invalid param index: " + param);
-//                result.add(paramMap);
-//            }
-//
-//            return ResponseEntity.ok(result);
-//        } catch (Exception e) {
-//            String eMessage = "An error occurred while retrieving data";
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-//        }
-//    }
 
     @PostMapping("/postgres")
     public ResponseEntity<Map<String, Object>> insertData(
@@ -107,9 +52,7 @@ public class ServiceController {
             @RequestBody List<Map<String, Object>> dataList
     ) {
         try {
-            System.out.println(dataList.size());
             String table = headers.getFirst("table-name");
-//            System.out.println(table);
             long startTime = System.currentTimeMillis(); // Waktu mulai
             serviceRepository.insertData(dataList, table);
             long endTime = System.currentTimeMillis(); // Waktu selesai
@@ -129,62 +72,6 @@ public class ServiceController {
                     .body(errorResponse);
         }
     }
-
-
-
-//    @PostMapping("/postgres")
-//    public ResponseEntity<String> insertData(
-//            @RequestHeader HttpHeaders headers,
-//            @RequestBody List<Map<String, Object>> dataList) {
-//        try {
-//
-//            List<String> key = new ArrayList<>();
-//            for (Map<String, Object> data : dataList) {
-//                key.addAll(data.keySet());
-//                break;
-//            }
-//            listToLowercase convToLow = new listToLowercase();
-//            List<String> lowKey = convToLow.listLowercase(key);
-//
-//            List<String> tableName = serviceRepository.getAllTableNames();
-//
-//            List<Boolean> isDuplicate = new ArrayList<>();
-//            for (String table : tableName) {
-//                List<String> column = convToLow.listLowercase(serviceRepository.getColumnList(table));
-//                boolean cmpr = CompareList.compareLists(lowKey, column);
-//                if (cmpr) {
-//                    isDuplicate.add(true);
-//                } else {
-//                    isDuplicate.add(false);
-//                }
-//            }
-//
-//            boolean createTable = !isDuplicate.contains(true);
-//
-//            String newTableName = headers.getFirst("table-name");
-//            System.out.println(newTableName);
-//
-//            if (createTable) {
-//                MapReader mapReader = new MapReader();
-//                String fileName = mapReader.cmprMapper(lowKey);
-//                if (fileName != null) {
-//                    List<String> columnList = mapReader.mapping(fileName);
-//                    serviceRepository.createTableWithMap(columnList, newTableName);
-//                    serviceRepository.insertData(dataList, newTableName);
-//                } else {
-//                    serviceRepository.createTable(key, newTableName);
-//                    serviceRepository.insertData(dataList, newTableName);
-//                }
-//            } else {
-//                serviceRepository.insertData(dataList, newTableName);
-//            }
-//            return ResponseEntity.ok("Data inserted succesfully!");
-//        } catch (Exception e) {
-//            String eMessage = "Failed to insert data!";
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                    .body(eMessage);
-//        }
-//    }
 
     @PostMapping("/postgres/create-table")
     public ResponseEntity<Map<String, Object>> createTable(
--- src/main/java/com/ujisistempostgres/repository/ServiceRepository.java
+++ src/main/java/com/ujisistempostgres/repository/ServiceRepository.java
@@ -15,28 +15,6 @@ public class ServiceRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Map<String, Object>> getAllData(String tableName) {
-        try {
-            String sql = String.format("SELECT * FROM %s", tableName);
-            return jdbcTemplate.query(sql, (resultSet, rowNum) -> {
-                ResultSetMetaData metaData = resultSet.getMetaData();
-                int columnCount = metaData.getColumnCount();
-
-                Map<String, Object> dataMap = new HashMap<>();
-                for (int i = 1; i <= columnCount; i++) {
-                    String columnName = metaData.getColumnName(i);
-                    Object columnValue = resultSet.getObject(i);
-                    dataMap.put(columnName, columnValue);
-                }
-
-                return dataMap;
-            });
-        } catch (DataAccessException e) {
-            e.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
     public List<List<Map<String, Object>>> getBothData(List<String> tableNames) {
         try {
             List<List<Map<String, Object>>> result = new ArrayList<>();
@@ -68,44 +46,6 @@ public class ServiceRepository {
         }
     }
 
-
-
-
-
-//    public void insertData(Map<String, Object> dataMap) {
-//        String tableName = "saleslineframe";
-//        String columns = String.join(", ", dataMap.keySet());
-//        String placeholders = String.join(", ", Collections.nCopies(dataMap.size(), "?"));
-//
-//        String sql = String.format("INSERT INTO %s (%s) VALUES (%s)", tableName, columns, placeholders);
-//        jdbcTemplate.update(sql, dataMap.values().toArray());
-//    }
-
-//    public void insertData(List<Map<String, Object>> dataList, String tableName) {
-//        try {
-//            if (dataList.isEmpty()) {
-//                System.out.println("Data list is empty.");
-//                return;
-//            }
-//
-//            Map<String, Object> firstRow = dataList.get(0);
-//            String columns = String.join(", ", firstRow.keySet());
-//            String placeholders = String.join(", ", Collections.nCopies(firstRow.size(), "?"));
-//
-//            List<Object[]> batchParams = new ArrayList<>();
-//            for (Map<String, Object> dataMap : dataList) {
-//                Object[] values = dataMap.values().toArray();
-//                batchParams.add(values);
-//            }
-//
-//            String sql = String.format("INSERT INTO %s (%s) VALUES (%s)", tableName, columns, placeholders);
-//            jdbcTemplate.batchUpdate(sql, batchParams);
-//        } catch (DataAccessException e) {
-//            System.out.println("Error executing SQL statement: " + e.getMessage());
-//            e.printStackTrace();
-//        }
-//    }
-
     public void insertData(List<Map<String,Object>> dataList, String tableName) {
         List<String> column = new ArrayList<>();
         for (Map<String, Object> data : dataList) {
@@ -127,7 +67,6 @@ public class ServiceRepository {
 
             allValue.add(wrapValue);
         }
-        System.out.println("cekkk");
 
         String joinAllValue = String.join(",", allValue);
 
@@ -141,43 +80,6 @@ public class ServiceRepository {
         return jdbcTemplate.queryForList(query, String.class);
     }
 
-    public List<String> getAllTableNamesForChoose() {
-        String query = "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public';";
-        List<Map<String, Object>> results = jdbcTemplate.queryForList(query);
-
-        List<String> tableNames = new ArrayList<>();
-        int count = 1;
-        for (Map<String, Object> row : results) {
-            String tableName = (String) row.get("tablename");
-            tableNames.add(count + ". " + tableName);
-            count++;
-        }
-
-        return tableNames;
-    }
-
-    public String[] getAllTableNamesForSelectByTable() {
-        String query = "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public';";
-        List<String> tableNameList = jdbcTemplate.queryForList(query, String.class);
-
-        String[] tableNameArray = new String[tableNameList.size()];
-        tableNameArray = tableNameList.toArray(tableNameArray);
-
-        return tableNameArray;
-    }
-
-    public List<String> getColumnList(String table) {
-        String sql = "SELECT column_name FROM information_schema.columns WHERE table_name = ?";
-        List<String> columnList = new ArrayList<>();
-
-        jdbcTemplate.query(sql, new Object[]{table}, (rs, rowNum) -> {
-            String columnName = rs.getString("column_name");
-            columnList.add(columnName);
-            return null;
-        });
-
-        return columnList;
-    }
 
     public void createTable(Map<String, Object> columnList, String tableName) {
         List<String> column = new ArrayList<>();
@@ -194,53 +96,8 @@ public class ServiceRepository {
         String cols = String.join(",", columnAndType);
 
         String sql = String.format("CREATE TABLE IF NOT EXISTS %s (%S)", tableName, cols);
-        System.out.println(sql);
         jdbcTemplate.update(sql);
     }
 
-    public void createTableWithMap(List<String> columns, String tableName) {
-        String column = String.join(",", columns);
-        String sql = String.format("CREATE TABLE IF NOT EXISTS %s (%S)", tableName, column);
-        System.out.println("Success create table");
-        jdbcTemplate.update(sql);
-    }
 
-    public void mencoba(List<Map<String, Object>> dataList) {
-        String insert = "INSERT INTO";
-        String table = "tb_cb";
-
-
-
-
-        List<String> column = new ArrayList<>();
-        for (Map<String, Object> data : dataList) {
-            column.addAll(data.keySet());
-            break;
-        }
-        String colName = String.join(",", column);
-        String template = insert + " " + table + " " + "(" + colName +")";
-
-        List<String> allValue = new ArrayList<>();
-        List<String> value = new ArrayList<>();
-        for (Map<String, Object> data : dataList) {
-            value.clear();
-            for (Object obj : data.values()) {
-                value.add("'" + obj.toString() + "'");
-            }
-            String joinValue = String.join(",", value);
-            String wrapValue = "(" + joinValue +")";
-
-            allValue.add(wrapValue);
-        }
-
-        String joinAllValue = String.join(",", allValue);
-
-        String sql = template + " VALUES " + joinAllValue;
-
-        System.out.println(sql);
-
-
-
-//        System.out.println(sql);
-    }
 }
